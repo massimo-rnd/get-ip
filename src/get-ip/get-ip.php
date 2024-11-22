150c586b51eba262eb0f5aaf7408e26999f3164d
druffko <christoph@rustige.me>
2024-11-22 22:09:33 +0100

    Implemented IPv6 Support, Fixed Hostname

--- src/get-ip/get-ip.php
+++ src/get-ip/get-ip.php
@@ -1,35 +1,44 @@
 <?php
 /*
 Plugin Name: Get-IP
-Plugin URI: https://github.com/druffko/get-ip
+Plugin URI: https://github.com/massimo-rnd/get-ip
 Description: A simple plugin to enable showing the current users IP address and hostname. You can use the shortcode [show_ip] to view the users IP address or [show_hostname] to show the users hostname on any page.
-Version: 1.0
+Version: 1.1
 Requires at least: 4.8
-Tested up to: 5.5
+Tested up to: 6.7.1
 Requires PHP: 5.6
-Author: druffko
-Author URI: https://druffko.gg
+Author: massimo-rnd
+Author URI: https://massimo.gg
 License: MIT
 
-Copyright (c) 2024 druffko. All rights reserved.
+Copyright (c) 2024 massimo-rnd. All rights reserved.
 */
 
 function get_ip() {
-	if ( ! empty( $_SERVER['HTTP_CLIENT_IP'] ) ) {
-	    $ip = $_SERVER['HTTP_CLIENT_IP'];
-	} elseif ( ! empty( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ) {
-	    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-	} else {
-	    $ip = $_SERVER['REMOTE_ADDR'];
-	}
-	    return apply_filters( 'wpb_get_ip', $ip );
-	}
+    if ( ! empty( $_SERVER['HTTP_CLIENT_IP'] ) ) {
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    } elseif ( ! empty( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ) {
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } else {
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+    if (filter_var($ip, FILTER_VALIDATE_IP)) {
+        return apply_filters( 'wpb_get_ip', $ip );
+    } else {
+        return 'Invalid IP Address';
+    }
+}
 
-	function get_hostname() {
-		$hostname = gethostbyaddr($_SERVER['REMOTE_ADDR']);
-	}
-	
+function get_hostname() {
+    $ip = get_ip();
 
-	add_shortcode('show_ip', 'get_ip');
-	add_shortcode('show_hostname', 'get_hostname')
-?>
+    if (filter_var($ip, FILTER_VALIDATE_IP)) {
+        $hostname = gethostbyaddr($ip);
+        return apply_filters( 'wpb_get_hostname', $hostname ? $hostname : 'Hostname not found');
+    } else {
+        return 'Invalid IP Address';
+    }
+}
+
+add_shortcode('show_ip', 'get_ip');
+add_shortcode('show_hostname', 'get_hostname');
